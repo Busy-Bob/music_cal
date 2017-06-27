33dc52b25594b7b622e3064fa332bab8330e8ca9
Busy-York <565862568@qq.com>
2017-06-27 12:16:15 +0800

    键盘还没写完

--- key_out.v
+++ key_out.v
@@ -1,4 +1,52 @@
-module key_out(input IN_clk, input [3:0] IN_value, input IN_key, 
-               output reg [3:0] OUT_col, output reg [7:0] OUT_SRCH, output reg [7:0] OUT_SRCL, output reg [7:0] OUT_DSTH, output reg [7:0] OUT_DSTL, output reg [3:0] OUT_ALU_OP, output reg OUT_finish);
+// s0等待输入
+// s1前一个数
+// s2符号
+// s3后一个数字
+// flag 表示数量
+module key_out(input IN_clk, input [3:0] IN_value, input IN_key, input IN_reset,
+              output reg [7:0] OUT_SRCH, output reg [7:0] OUT_SRCL, output reg [7:0] OUT_DSTH, output reg [7:0] OUT_DSTL, output reg [3:0] OUT_ALU_OP, output reg OUT_finish);
 
+
+	reg [15:0] temp = 0;
+	reg [1:0] flag = 0;
+	
+	always @(posedge IN_clk or negedge IN_reset)
+	begin
+		if(!IN_reset)
+			state = s0;
+		else if(IN_key)
+			case(state)
+				s0: begin
+					if(IN_value > 4'h9)
+					begin
+						OUT_SRCL = 
+					end
+					state = s1;
+				end
+				s1: begin
+					if(IN_value > 4'h9)
+					begin
+						state = s2;
+						OUT_ALU_OP = IN_value;
+					end
+					else
+					begin
+					if(flag < 2'd3)
+					begin
+						temp = temp * 3'd10 + IN_value;
+						flag = flag + 2'b01;
+					end
+					else
+					begin
+						temp = temp;
+						flag = flag;
+					end
+					end
+				end
+		else
+		
+			endcase
+		
+	end
+				  
 endmodule 
--- keyboard.v
+++ keyboard.v
@@ -6,186 +6,141 @@
 // s3 第四列 +(A) -(B) and(C) or(D) 
 
 module keyboard(input IN_clk, input[3:0] IN_row,
-					output reg [3:0] OUT_col, output reg [7:0] OUT_SRCH, output reg [7:0] OUT_SRCL, output reg [7:0] OUT_DSTH, output reg [7:0] OUT_SRCL, output reg [7:0] OUT_ALU_OP, output reg OUT_finish);
+                    output reg [3:0] OUT_col, output reg [3:0] OUT_value, output reg OUT_key);
 
-	// Declare state register
-	reg	[2:0] state = 0;
-	reg [3:0] data = 0;
-	reg key = 0;
-	
-	parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5;
+    // Declare state register
+    reg [1:0]state = 0;
+    reg [1:0]flag = 0;
 
-	always @ (posedge IN_clk) 
-	begin
-			case (state)
-				S0:
-					begin
-						if(IN_row[1])
-						begin
-							state = S1;
-						end
-						else if(IN_row[2])
-						begin
-							state = S2;
-						end
-						else if(IN_row[3])
-						begin
-							state = S3;
-						end
-						else if(IN_row[4])
-						begin
-							state = S4;
-						end
-						else
-						begin
-							state = S0;
-							key = 1'b0;
-							data = data;
-							OUT_col = 4'b1111;
-						end					
-					end
-				S1:
-					begin
-						key = 1'b0;
-						if(IN_row[4])
-							state = S1;
-						else
-							state = S0;
-					end
-				S2:
-					begin
-						key = 1'b0;
-						if(IN_row[3])
-							state = S2;
-						else
-							state = S0;
-					end
-				S3:
-					begin
-						key = 1'b0;
-						if(IN_row[2])
-							state = S3;
-						else
-							state = S0;
-					end
-				S4:
-					begin
-						key = 1'b0;
-						if(IN_row[1])
-							state = S4;
-						else
-							state = S0;
-					end
-				default:
-					begin
-						key = 1'b0;
-						data = 4'b0000;
-						OUT_col = 4'b1111;
-						state = S0;
-					end
-			endcase
-	end
+    // Declare  states
+    parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3;
 
-	// Determine the output based only on the current state
-	// and the input (do not wait for a clock edge).
-	// always @ (state or IN_reset or IN_row)
-	always @ (state)
-	begin
-		//OUT_value <= OUT_value;
-		begin
-			case (state)
-				S0：
-				begin
-					key = 1'b0;
-					OUT_col = 4'b1111;
-				end
-				S1:
-					case(IN_row)
-						4'b1000: begin OUT_value <= 1; OUT_key <= 1; flag <= 0; end
-						4'b0100: begin OUT_value <= 4; OUT_key <= 1; flag <= 0; end
-						4'b0010: begin OUT_value <= 7; OUT_key <= 1; flag <= 0; end
-						4'b0001: begin OUT_value <= 0; OUT_key <= 1; flag <= 0; end
-						default: begin 
-							OUT_value <= OUT_value; 
-							if(flag != 3)begin
-								OUT_key <= OUT_key; 
-								flag <= flag + 1;
-							end
-							else
-							begin
-								OUT_key <= 0;
-								flag <= 0;
-							end
-						end
-					endcase
-				S1:
-					case(IN_row)
-						4'b1000: begin OUT_value <= 2; OUT_key <= 1; flag <= 0; end
-						4'b0100: begin OUT_value <= 5; OUT_key <= 1; flag <= 0; end
-						4'b0010: begin OUT_value <= 8; OUT_key <= 1; flag <= 0; end
-						4'b0001: begin OUT_value <= 15; OUT_key <= 1; flag <= 0; end
-						default: begin 
-							OUT_value <= OUT_value; 
-							if(flag != 3)begin
-								OUT_key <= OUT_key; 
-								flag <= flag + 1;
-							end
-							else
-							begin
-								OUT_key <= 0;
-								flag <= 0;
-							end
-						end
-					endcase
-				S2:
-					case(IN_row)
-						4'b1000: begin OUT_value <= 3; OUT_key <= 1; flag <= 0; end
-						4'b0100: begin OUT_value <= 6; OUT_key <= 1; flag <= 0; end
-						4'b0010: begin OUT_value <= 9; OUT_key <= 1; flag <= 0; end
-						4'b0001: begin OUT_value <= 14; OUT_key <= 1; flag <= 0; end
-						default: begin 
-							OUT_value <= OUT_value; 
-							if(flag != 3)begin
-								OUT_key <= OUT_key; 
-								flag <= flag + 1;
-							end
-							else
-							begin
-								OUT_key <= 0;
-								flag <= 0;
-							end
-						end
-					endcase
-					
-				S3:
-					case(IN_row)
-						4'b0001: begin OUT_value <= 13; OUT_key <= 1; flag <= 0; end
-						default: begin 
-							OUT_value <= OUT_value; 
-							if(flag != 3)begin
-								OUT_key <= OUT_key; 
-								flag <= flag + 1;
-							end
-							else
-							begin
-								OUT_key <= 0;
-								flag <= 0;
-							end
-						end
-					endcase
-				default:
-					begin OUT_value <= 0; OUT_key <= 0; end
-			endcase
-		end
+    always @ (posedge IN_clk) 
+    begin
+            case (state)
+                S0:
+                    begin
+                        state <= S1;
+                        OUT_col <= 4'b0100;
+                    end
+                S1:
+                    begin
+                        state <= S2;
+                        OUT_col <= 4'b0010;
+                    end
+                S2:
+                    begin
+                        state <= S3;
+                        OUT_col <= 4'b0001;
+                    end
+                S3:
+                    begin
+                        state <= S0;
+                        OUT_col <= 4'b1000;
+                    end
+                default:
+                    begin
+                        state <= S0;
+                        OUT_col <= 4'b1000;
+                    end
+            endcase
+    end
+
+    // Determine the output based only on the current state
+    // and the input (do not wait for a clock edge).
+    // always @ (state or IN_reset or IN_row)
+    always @ (posedge IN_clk)
+    begin
+        //OUT_value <= OUT_value;
+        begin
+            case (state)
+                S0:
+                    case(IN_row)
+                        4'b1000: begin OUT_value <= 1; OUT_key <= 1; flag <= 0; end
+                        4'b0100: begin OUT_value <= 4; OUT_key <= 1; flag <= 0; end
+                        4'b0010: begin OUT_value <= 7; OUT_key <= 1; flag <= 0; end
+                        4'b0001: begin OUT_value <= 0; OUT_key <= 1; flag <= 0; end
+                        default: begin 
+                            OUT_value <= OUT_value; 
+                            if(flag != 3)begin
+                                OUT_key <= OUT_key; 
+                                flag <= flag + 1;
+                            end
+                            else
+                            begin
+                                OUT_key <= 0;
+                                flag <= 0;
+                            end
+                        end
+                    endcase
+                S1:
+                    case(IN_row)
+                        4'b1000: begin OUT_value <= 2; OUT_key <= 1; flag <= 0; end
+                        4'b0100: begin OUT_value <= 5; OUT_key <= 1; flag <= 0; end
+                        4'b0010: begin OUT_value <= 8; OUT_key <= 1; flag <= 0; end
+                        4'b0001: begin OUT_value <= 15; OUT_key <= 1; flag <= 0; end
+                        default: begin 
+                            OUT_value <= OUT_value; 
+                            if(flag != 3)begin
+                                OUT_key <= OUT_key; 
+                                flag <= flag + 1;
+                            end
+                            else
+                            begin
+                                OUT_key <= 0;
+                                flag <= 0;
+                            end
+                        end
+                    endcase
+                S2:
+                    case(IN_row)
+                        4'b1000: begin OUT_value <= 3; OUT_key <= 1; flag <= 0; end
+                        4'b0100: begin OUT_value <= 6; OUT_key <= 1; flag <= 0; end
+                        4'b0010: begin OUT_value <= 9; OUT_key <= 1; flag <= 0; end
+                        4'b0001: begin OUT_value <= 14; OUT_key <= 1; flag <= 0; end
+                        default: begin
+                            OUT_value <= OUT_value;
+                            if(flag != 3)begin
+                                OUT_key <= OUT_key; 
+                                flag <= flag + 1;
+                            end
+                            else
+                            begin
+                                OUT_key <= 0;
+                                flag <= 0;
+                            end
+                        end
+                    endcase
+                    
+                S3:
+                    case(IN_row)
+                        4'b0001: begin OUT_value <= 13; OUT_key <= 1; flag <= 0; end
+                        default: begin 
+                            OUT_value <= OUT_value; 
+                            if(flag != 3)begin
+                                OUT_key <= OUT_key; 
+                                flag <= flag + 1;
+                            end
+                            else
+                            begin
+                                OUT_key <= 0;
+                                flag <= 0;
+                            end
+                        end
+                    endcase
+                default:
+                    begin OUT_value <= 0; OUT_key <= 0; end
+            endcase
+        end
 
 /*
-	always @(IN_reset)
-	begin
-	if(IN_reset)
-		deassign OUT_value;
-	else
-		assign OUT_value = 0;
-	end
+    always @(IN_reset)
+    begin
+    if(IN_reset)
+        deassign OUT_value;
+    else
+        assign OUT_value = 0;
+    end
 */
 end
-	endmodule
+    endmodule
